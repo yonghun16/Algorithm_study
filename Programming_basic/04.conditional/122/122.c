30dd8e24062ccb6a476037d21d9c5f7b4483be44
yonghun16 <yonghun16@naver.com>
2022-04-09 23:23:27 +0900

    problems

--- Programming_basic/04.conditional/122/122.c
+++ Programming_basic/04.conditional/122/122.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main(void)
 {
     int year;
     scanf("%d", &year);
-    year = !(year%400) || ( !(year%4) && (year%100) );
-    if(year) printf("leap year");
+    if (year % 400 == 0 || (year % 4 ==0 && year % 100 != 0)) printf("leap year");
     else printf("common year");
-
-    printf("%d", year);
 
     return 0;
 }
